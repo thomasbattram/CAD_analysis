17e4b9dc9bc11296ba4b70b59bb2ead2c012d3cf
Thomas Battram <tb13101@bristol.ac.uk>
2017-11-07 09:33:14 +0000

    changed comments and added prerequisites

--- Masterfile.R
+++ Masterfile.R
@@ -1,37 +1,45 @@
-######################
-# Masterfile #########
-######################
+# ------------------------------------------------------------------
+# Masterfile 
+# ------------------------------------------------------------------
 rm(list = ls())
 
-#Age the analysis is to be conducted at
-#If all ages make age "all"
+# Prerequisites:
+# - These folders: inputs, outputs and R
+# - All R files from github should be deposited in R folder
+# - All packages should be installed from the setup.R script
+
+
+
+# Select the age at which the analysis is to be conducted at
+# If all ages make age "all"
 age <- "all"
 #age <- 7
 #age <- 15
 #age <- 17
 
-#########
-# Setup #
-#########
+# ------------------------------------------------------------------
+# Setup 
+# ------------------------------------------------------------------
 source("R/setup.R")
 
-############
-# Analysis #
-############
+# ------------------------------------------------------------------
+# Analysis 
+# ------------------------------------------------------------------
 
-#Analysis of association between CAD-GRS and metabolites
+# Analysis of association between CAD-GRS and metabolites
 source("R/CAD_GRS_analysis.R")
 
-#Analysis of association between all variants and metabolites
+# Analysis of association between all variants and metabolites
 source("R/Individual_variant_analysis.R")
 
-#Analysis of association between new GRS (formed from SNPs that associate with metabolites) and metabolites
-#with comparison to original CAD-GRS
+# Analysis of association between new GRS (formed from SNPs that associate with metabolites) and metabolites
+# with comparison to original CAD-GRS
 source("R/New_GRS_analysis.R")
 
-#Analysis of association between SNPs within HMGCR (proxies for statin use) and metabolites
+# Analysis of association between SNPs within HMGCR (proxies for statin use) and metabolites
 source("R/HMGCR_SNP_analysis.R")
 
 
 #All ggplot graphs - larger writing
 #Heatmaps - add in gene names
+
