eb723f5d6d393507080c9ba4698c2bd0bab31036
Thomas Battram <tb13101@bristol.ac.uk>
2017-11-09 14:27:25 +0000

    Tidied script and made sure it could run with only setup.R being run before

--- Age_sensitivity_analyses.R
+++ Age_sensitivity_analyses.R
@@ -1,8 +1,8 @@
-#############################
-# Age sensitivity analyses ##
-#############################
+# ----------------------------------------------------------------------------
+# Age sensitivity analyses
+# ----------------------------------------------------------------------------
 
-#Make sure to complete all the parts in the masterfile before starting 
+# Make sure to complete all the parts in the masterfile before starting 
 
 #So far it seems as though there is potentially a difference between those at aged 7, 15 and 17
 # -little difference between 15 and 17
@@ -12,19 +12,19 @@
 # -graphs also seem to suggest subtle differences
 # -SOLUTION: there are many people aged 7, could just have the main analysis done at that age and then subsequent analyses done at age 15 and 17
 
+# ----------------------------------------------------------------------------
+# Setup of data
+# ----------------------------------------------------------------------------
 
-
-
-#Comparison of metabolite concentrations 
+# Comparison of metabolite concentrations 
 met_dat <- df_main_not_transformed %>%
 	dplyr::select(u_ID, age, everything())
-temp <- summary(met_dat[,3])
-summary(lm(met_dat[,3] ~ met_dat[,4]))
+temp <- summary(met_dat[, 3])
 str_split(temp[1, ], ":")[[1]][2]
 
 data <- met_dat[, 3]
 
-
+# Split the metabolite data into different age groups
 age_diff <- list()
 age_diff_not_log <- list()
 for (i in unique(met_dat[["age"]])) {
@@ -52,6 +52,11 @@ for (i in unique(met_dat[["age"]])) {
 	age_diff_not_log[[as.character(i)]] <- temp_out
 }
 
+# ----------------------------------------------------------------------------
+# Metabolite concentration 
+# ----------------------------------------------------------------------------
+
+# Remove metabolites that are particle sizes - values too different to put on same graph
 particle_mets <- nr_mnames[grep("-P$", nr_mnames)]
 result <- age_diff
 for (i in names(result)) {
@@ -69,15 +74,13 @@ forest_dat <- do.call(rbind, result) %>%
 	mutate(age = rep(c("7", "15", "17"), each = nrow(result[[1]]))) %>%
 	mutate(facet_var = facet_var)
 
-###PRODUCES A FOREST THAT ALLOWS VISUALISATION OF DATA - the metabolite conc differences are too large
-# Still not completely right
+# Could be changed still? 
 forest_dat[["facet_var"]] <- as.factor(forest_dat[["facet_var"]])
 pdf("outputs/forests/Age_diff_met_conc_no_particle_forest.pdf", width = 15, height = 10)
 forest_plot(forest_dat, col_num = 4, group = "age", y_axis_var = "Metabolite", units = "Log10(median metabolite concentration)")
 dev.off()
 
-#--------- Particle sizes only
-
+# Particle sizes only
 result <- age_diff
 for (i in names(result)) {
 	result[[i]] <- result[[i]] %>%
@@ -88,21 +91,19 @@ stopifnot(result[[1]][["Metabolite"]] == result[[2]][["Metabolite"]] && result[[
 #Need to change facet_var_gen()
 facet_var <- facet_var_gen(result[[1]], col_num = 2, group_num = 3)
 
-
-
 forest_dat <- do.call(rbind, result) %>%
 	mutate(age = rep(c("7", "15", "17"), each = nrow(result[[1]]))) %>%
 	mutate(facet_var = facet_var)
 
-
-###PRODUCES A FOREST THAT ALLOWS VISUALISATION OF DATA - the metabolite conc differences are too large
-# Still not completely right
+# Could be changed still? 
 forest_dat[["facet_var"]] <- as.factor(forest_dat[["facet_var"]])
 pdf("outputs/forests/Age_diff_met_conc_particle_only_forest.pdf", width = 15, height = 10)
 forest_plot(forest_dat, col_num = 2, group = "age", y_axis_var = "Metabolite", units = "Log10(median metabolite concentration)")
 dev.off()
 
-#--------------------------------
+# ----------------------------------------------------------------------------
+# GRS-metabolite association 
+# ----------------------------------------------------------------------------
 
 full_dat <- do.call(rbind, age_diff)
 full_dat$age <- c(rep(7, 149), rep(15, 149), rep(17, 149))
@@ -111,6 +112,10 @@ kw_age_test <- kruskal.test(Estimate ~ age, data = full_dat)
 kw_age_test #p = 0.8227 therefore no evidence of difference between log10 median values of each metabolite at different age groups
 
 colnames(met_dat)
+
+if (!("CAD_score" %in% colnames(d))) {
+	d[["CAD_score"]] <- rowSums(d[, SNPs])
+}
 
 #Comparison of metabolite-GRS associations
 age_diff_reg <-  list()
@@ -142,25 +147,10 @@ kw_age_test2 <- kruskal.test(Estimate ~ age, data = full_dat2)
 kw_age_test2 #p = 0.007643 therefore evidence of difference between age groups 
 dunn_age_test <- dunnTest(Estimate ~ as.factor(age), data = full_dat2, method = "bh")
 
-#For some reason the p vals are all the same - no residuals? 
-met_dunn_tests <- list()
-for (i in nr_mnames[!nr_mnames %in% c("insulin", "glucose")]) {
-	print(i)
-	temp_dat <- subset(full_dat2, Metabolite == i)
-	temp_kw <- kruskal.test(Estimate ~ age, data = temp_dat)
-	if (temp_kw[[3]] < 0.05) {
-		print(paste("There is evidence to suggest the association between the GRS and ", i, " differs depending on age", sep = ""))
-		temp_dunn <- dunnTest(Estimate ~ as.factor(age), data = temp_dat, method = "bh")
-		met_dunn_tests[[i]] <- temp_dunn
-	}
-}
-hist(age_diff_reg[[1]][["Estimate"]])
-hist(age_diff_reg[[2]][["Estimate"]])
-hist(age_diff_reg[[3]][["Estimate"]])
 
-
-
-
+# Kettunen GWAS res
+met_GWAS <- read.delim("inputs/Kettunen_met_GWAS.tsv")
+head(met_GWAS)
 
 
 
