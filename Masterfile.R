3eb4f057b2b76a440451bbb602b8fdcda26c9f29
Thomas Battram <tb13101@bristol.ac.uk>
2017-11-10 14:49:53 +0000

    updated to-do list

--- Masterfile.R
+++ Masterfile.R
@@ -9,8 +9,6 @@ rm(list = ls())
 # - All packages should be installed from the setup.R script
 # - Run the Individual_variant_analysis.R script once before the New_GRS_analysis.R script
 
-
-
 # Select the age at which the analysis is to be conducted at
 # If all ages make age "all"
 age <- "all"
@@ -51,13 +49,13 @@ source("R/Age_sensitivity_analyses.R")
 # Add in the age sensitivity analysis |+|
 # Add in the metabolite GWAS into the age sensitivity analysis
 # Change the age sensitivity analysis so it looks nice and clear
-# Go through to see how genetic data and metabolite data is read in
+# Go through to see how genetic data and metabolite data is read in |+|
 # Check the HMGCR step in setup - why does there need to be a variable HMGCR_SNPs?
-# Maybe change the for loop in the lipoprotein groups bit - little confusing - could just label it better?
-# Change the heatmap production section so it produces all 4 in a smaller amount of code
-# Add new control check for biology-driven clustered heatmaps - at the moment uses "temp_dat" which is not good...
+# Maybe change the for loop in the lipoprotein groups bit - little confusing - could just label it better? |+|
+# Change the heatmap production section so it produces all 4 in a smaller amount of code |+|
+# Add new control check for biology-driven clustered heatmaps - at the moment uses "temp_dat" which is not good... |+|
 # Find a way to add gene names to heatmaps without them going off the edge - then add gene names to all heatmaps
-# Put linear regression function in setup.R 
+# Put linear regression function in setup.R |+|
 # Check they all run in sequence again
 # Remove prerequisites from Masterfile.R and put them into the README
 # Need to change Dendrogram_production.R if keeping the different ages thing...
