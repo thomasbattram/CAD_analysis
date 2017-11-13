b268dedf7f8c48490aaa19731e8fa63b1827c515
Thomas Battram <tb13101@bristol.ac.uk>
2017-11-13 15:00:18 +0000

    Changed to-do list and removed prerequisites

--- Masterfile.R
+++ Masterfile.R
@@ -3,12 +3,6 @@
 # ------------------------------------------------------------------
 rm(list = ls())
 
-# Prerequisites:
-# - These folders: inputs, outputs and R
-# - All R files from github should be deposited in R folder
-# - All packages should be installed from the setup.R script
-# - Run the Individual_variant_analysis.R script once before the New_GRS_analysis.R script
-
 # Select the age at which the analysis is to be conducted at
 # If all ages make age "all"
 age <- "all"
@@ -39,25 +33,14 @@ source("R/New_GRS_analysis.R")
 # Analysis of association between SNPs within HMGCR (proxies for statin use) and metabolites
 source("R/HMGCR_SNP_analysis.R")
 
-# Age sensitivity analysis
+# Age sensitivity analysis - STILL WORKING ON IT! 
 source("R/Age_sensitivity_analyses.R")
 
 
-# Go through scripts and make them tidier |+|
-# Check that they all run in sequence |+|
-# Make it so that setup is the only thing that needs to be ran and each of the analysis steps can be ran without running the other steps |+|
-# Add in the age sensitivity analysis |+|
 # Add in the metabolite GWAS into the age sensitivity analysis
 # Change the age sensitivity analysis so it looks nice and clear
-# Go through to see how genetic data and metabolite data is read in |+|
-# Check the HMGCR step in setup - why does there need to be a variable HMGCR_SNPs?
-# Maybe change the for loop in the lipoprotein groups bit - little confusing - could just label it better? |+|
-# Change the heatmap production section so it produces all 4 in a smaller amount of code |+|
-# Add new control check for biology-driven clustered heatmaps - at the moment uses "temp_dat" which is not good... |+|
 # Find a way to add gene names to heatmaps without them going off the edge - then add gene names to all heatmaps
-# Put linear regression function in setup.R |+|
-# Check they all run in sequence again
-# Remove prerequisites from Masterfile.R and put them into the README
+# Check they all run in sequence again 
 # Need to change Dendrogram_production.R if keeping the different ages thing...
 
 
